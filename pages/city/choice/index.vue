b2de5ae1b9916bfc44ecacc6f72cb8f92c938153
sunhui44311 <1874314758@qq.com>
2021-02-22 14:14:38 +0800

    updata

---
+++ pages/city/choice/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <view id="choice">
+    <u-index-list :scrollTop="scrollTop">
+      <view v-for="(item, index) in indexList" :key="index">
+        <u-index-anchor :index="item" />
+        <view class="list-cell" v-for="(i, j) in screen(item)" :key="j">
+          {{ i.name }}
+        </view>
+      </view>
+    </u-index-list>
+  </view>
+</template>
+<script src="./index.js"></script>
+<style lang="scss" scoped>
+@import "index.scss";
+</style>
